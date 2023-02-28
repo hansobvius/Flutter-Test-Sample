78ee419b90d8b2e8d58289b1c54b08fc3673df06
Thiago Caiado <thiago.caiado@hotmail.com>
2023-02-28 11:03:40 -0300

    items list views integration test

--- integration_test/single_integration_test/list_item_module/list_item_module_integration_test.dart
+++ integration_test/single_integration_test/list_item_module/list_item_module_integration_test.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_driver_sample/application/module/list_items/ui/list_items_view.dart';
-import 'package:flutter_driver_sample/application/module/login/controller/login_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
-import 'package:flutter_driver_sample/main.dart' as app;
-
 import '../../../test/widget_test/base_widget_test_launcher.dart';
 
 void main() {
 
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('LIST ITEM VIEW', () {
+  group('List item widget test', () {
 
     testWidgets('Test generated list', (WidgetTester tester) async {
 
       await tester.runAsync(() async {
 
-        // Initialize
+        /// Initialize
         await tester.pumpWidget(initializeSingleView(const ListItemsView()));
         await tester.pump();
 
@@ -41,29 +37,30 @@ void main() {
           var firstListTile = find.byKey(const Key('0_LIST_TILE'));
           expect(firstListTile, findsOneWidget, reason: 'Check if first list element exists');
 
-          /// FIXME - doesn't work
-          // final listViewFinder = find.byType(ListView);
-          // final listViewHeight = tester.getSize(listViewFinder).height;
-          // await tester.scrollUntilVisible(
-          //     listViewFinder,
-          //     1.0 ,
-          //     scrollable: find.byType(ListTile),
-          //     duration: const Duration(seconds: 1));
-
           final listViewFinder = find.byType(ListView);
+
           final listViewHeight = tester.getSize(listViewFinder).height;
-          print('LIST_ITEM_HEIGHT $listViewHeight');
+          final listTileHeight = tester.getSize(firstListTile).height;
+
+          /// Calculation to retrieve how many scrolls count are needed
+          /// to reach last element presented from list view
+          final totalItemsPerView = listViewHeight / (listTileHeight + 16.0);
+          final maxScrollCounts = 1000 / totalItemsPerView;
+
+          final scrollableFinder = find
+              .descendant(of: listViewFinder, matching: find.byType(Scrollable));
+
+          final itemFinder = find.byKey(const ValueKey('999_LIST_TILE'));
+
           await tester.scrollUntilVisible(
-              listViewFinder,
-              listViewHeight * 1000,
-              duration: const Duration(seconds: 1));
+            itemFinder,
+            listViewHeight,
+            maxScrolls: maxScrollCounts.toInt(),
+            scrollable: scrollableFinder,
+          );
 
-          // await tester.scrollUntilVisible(find.byType(ListView), 1000.0);
-
-          // var lastListTile = find.byKey(const Key('1000_LIST_TILE'));
-          var lastListTile = find.byType(ListTile).last;
-          print('LIST_TILE_KEY ${lastListTile.evaluate()}');
-          expect(lastListTile, findsOneWidget, reason: 'Check if last list element exists');
+          expect(itemFinder, findsOneWidget,
+              reason: 'Check if last element from data list are presented on list view widget');
         });
       });
 
